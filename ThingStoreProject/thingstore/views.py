954f5f9ebc0b669ac02ef59d6cd2d7a07d18e1bc
Severin Schols <severin@schols.de>
2013-10-05 10:50:55 +0200

    don't fetch all values for graph, only required timeframe (12h for now)

--- ThingStoreProject/thingstore/views.py
+++ ThingStoreProject/thingstore/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.utils.timezone import now
+from datetime import timedelta
 
-from thingstore.models import Thing
+from thingstore.models import Thing, Value
 from django.contrib.auth.models import User
 
 def index(request):
@@ -11,8 +13,34 @@ def index(request):
 	return render(request, 'thingstore/index.html', context)
 	
 def thing(request, thing_id):
+	timeframe_hours = 12
+	
 	thing = get_object_or_404(Thing, pk=thing_id)
-	return render(request, 'thingstore/thing.html', {'thing': thing})
+	metrics = thing.metrics.all()
+	values = Value.objects.filter(metric__in = metrics, timestamp__gt = now()-timedelta(hours=timeframe_hours))
+	
+	relation_dict = {}
+	for value in values:
+		value_dict = value.__dict__
+		value_dict['js_time'] = value.js_time
+		relation_dict.setdefault(value.metric_id, []).append(value_dict)
+	
+	metrics_list = []
+	for metric in metrics:
+		metric_dict = metric.__dict__
+		metric_dict['current_value'] = metric.current_value
+		metric_dict['value_dict'] = relation_dict[metric.id]
+		metrics_list.append(metric_dict)
+
+	print metrics_list
+	
+	return render(request, 'thingstore/thing.html',
+		{
+			'thing': thing,
+			'metrics': metrics_list,
+			'timeframe_hours': timeframe_hours
+		}
+	)	
 
 def user(request, username):
 	user = get_object_or_404(User, username__iexact=username)
