51722a3dbc433ec9317aaf36c14a64cc22963d69
Severin Schols <severin@schols.de>
2014-01-23 00:29:38 +0100

    Added django-registration as backend for user registraion and login/-out
    Followed guide at http://www.michelepasin.org/blog/2011/01/14/setting-up-django-registration/ and used templates from there as well. Need to customize later.

--- ThingStoreProject/thingstore/urls.py
+++ ThingStoreProject/thingstore/urls.py
@@ -5,8 +5,9 @@ from thingstore import views,api
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
-    url(r'^login/$', views.login_view, name='login'),
-    url(r'^logout/$', views.logout_view, name='logout'),
+    # url(r'^login/$', views.login_view, name='login'),
+    # url(r'^logout/$', views.logout_view, name='logout'),
+    url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^thing/(?P<thing_id>\d+)/$', views.thing, name='thing'),
     url(r'^user/(?P<username>\w+)/$', views.user, name='user'),
     
