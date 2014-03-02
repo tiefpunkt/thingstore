fc190d7d143a25aaf483fa1e9334a76b05f82243
Severin Schols <severin@schols.de>
2014-03-02 21:18:28 +0100

    heroku adaptations

--- ThingStoreProject/settings.py
+++ ThingStoreProject/settings.py
@@ -173,3 +173,24 @@ TEMPLATE_CONTEXT_PROCESSORS = ('django.core.context_processors.request',
 ACCOUNT_ACTIVATION_DAYS = 7
 
 LOGIN_REDIRECT_URL = "/"
+
+## HEROKU
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
